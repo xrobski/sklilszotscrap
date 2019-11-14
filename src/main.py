061656effc31ff62223a11ad9f0938139a214db1
Robski <chrzmichal@gmail.com>
2019-11-14 15:59:54 +0100

    update next page function

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 class Jobs:
     def __init__(self):
         # Hardcode link for scrapping an offers
-        self.actual_page = "https://www.skillshot.pl/"
+        self.actual_page = "http://www.skillshot.pl/jobs?page=6"
         self.connection = requests.get(self.actual_page)
         self.src = self.connection.content
         self.soup = BeautifulSoup(self.src, 'lxml')
@@ -14,15 +14,16 @@ class Jobs:
         self.links = []
         self.page_counter = len(self.soup.find_all("a", class_="page-link"))
 
-        # Function that's scrapping for links
+        # Scrapping page for job links
     def scrapping_page(self):
         for link in self.soup.find_all(href=re.compile(r"/jobs/\d+")):
             self.links.append(('{}{}'.format('www.skillshot.pl', link.get('href'))))
 
+        # Change actual page
     def next_page(self):
         if self.soup.find_all("a", class_="page-link", rel="next"):
-            print(self.page_counter)
-
+            self.actual_page ="http://www.skillshot.pl"+ self.soup.find_all("a", class_="page-link", rel="next")[0].get('href')
+            print(self.actual_page)
 
         # for page in self.soup.find_all("a", class_="page-link", rel="next"):
         #     self.pages.add(page.get('href'))
