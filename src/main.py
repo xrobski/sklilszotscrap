f99f23ef7e4fac66a39193ffd122e447a98e5bc8
Robski <chrzmichal@gmail.com>
2019-11-14 15:37:44 +0100

    create basic structure

--- src/main.py
+++ src/main.py
@@ -1,32 +1,38 @@
-## 1 cylkl programu ##
-# 1) nawiązywanie połączenia ze stroną
-# 2) jeżeli kod połączenia to 200 przechodzę dalej
-# 3) wyszukuje wszystkie pozycje w danej kategorii
-# 4) zaczynam iterować po każdej z nich
-    # 5) jeżeli pozycja jest nowa - tworzę nowy wpis w bazie
-    # 6) jeżeli pozycja jest stara ale różni się aktualizuje
-    # 7) jeżeli pozycja jest stara ale nie różni się pomijam
-
-
+import re
 import requests
 from bs4 import BeautifulSoup
 
-#print(result.status_code)
 
-class Offers:
-    connection = requests.get("https://www.skillshot.pl/jobs?category=grafika")
+class Jobs:
+    def __init__(self):
+        # Hardcode link for scrapping an offers
+        self.actual_page = "https://www.skillshot.pl/"
+        self.connection = requests.get(self.actual_page)
+        self.src = self.connection.content
+        self.soup = BeautifulSoup(self.src, 'lxml')
 
-    def __init__(self, title):
-        #, link, publication_date, category, views
-        self.title = title
-        #self.link = link
-        #self.publication_date = publication_date
-        #self.category = category
-        #self.views = views
+        self.links = []
+        self.page_counter = len(self.soup.find_all("a", class_="page-link"))
 
-    def scrapp_a_website(self):
-        print(self.connection)
+        # Function that's scrapping for links
+    def scrapping_page(self):
+        for link in self.soup.find_all(href=re.compile(r"/jobs/\d+")):
+            self.links.append(('{}{}'.format('www.skillshot.pl', link.get('href'))))
+
+    def next_page(self):
+        if self.soup.find_all("a", class_="page-link", rel="next"):
+            print(self.page_counter)
 
 
-first_offer = Offers("abc")
-print(first_offer.scrapp_a_website())
+        # for page in self.soup.find_all("a", class_="page-link", rel="next"):
+        #     self.pages.add(page.get('href'))
+        # print(self.pages)
+
+    # def process_of_scrapping(self):
+    #     for x in range():
+    #         self.scrapp_a_website_for_links
+    #         self.change_page
+
+test = Jobs()
+print(test.next_page())
+# print(test.scrapp_a_website_for_links())
